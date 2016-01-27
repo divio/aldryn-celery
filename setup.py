0a1114ba8f31a83372d4cdda85d88d8d8093bb26
Stefan Foulis <stefan@foulis.ch>
2016-01-27 11:07:48 +0100

    extract package version without importing

--- setup.py
+++ setup.py
@@ -1,10 +1,41 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-from aldryn_celery import __version__
+
+
+# from https://gist.github.com/techtonik/4066623
+def get_version(relpath):
+    """Read version info from a file without importing it"""
+    from os.path import dirname, join
+
+    if '__file__' not in globals():
+        # Allow to use function interactively
+        root = '.'
+    else:
+        root = dirname(__file__)
+
+    # The code below reads text file with unknown encoding in
+    # in Python2/3 compatible way. Reading this text file
+    # without specifying encoding will fail in Python 3 on some
+    # systems (see http://goo.gl/5XmOH). Specifying encoding as
+    # open() parameter is incompatible with Python 2
+
+    # cp437 is the encoding without missing points, safe against:
+    #   UnicodeDecodeError: 'charmap' codec can't decode byte...
+
+    for line in open(join(root, relpath), 'rb'):
+        line = line.decode('cp437')
+        if '__version__' in line:
+            if '"' in line:
+                # __version__ = "0.9"
+                return line.split('"')[1]
+            elif "'" in line:
+                return line.split("'")[1]
+
+
 
 setup(
     name="aldryn-celery",
-    version=__version__,
+    version=get_version('aldryn_celery/__init__.py'),
     description='An opinionated Celery setup bundled as an Aldryn Addon. To be used together with aldryn-django.',
     author='Divio AG',
     author_email='info@divio.ch',

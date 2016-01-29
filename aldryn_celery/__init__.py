91b9f8b2b59c24286f0e9ed0afb568046359f7ce
Stefan Foulis <stefan@foulis.ch>
2016-01-29 11:31:54 +0100

    release 0.4

--- aldryn_celery/__init__.py
+++ aldryn_celery/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 
-__version__ = '0.3'
+__version__ = '0.4'
 
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.

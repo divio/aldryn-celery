1d27998828eb88133e77cc69563338bf6d49de5f
Kim Thoenen <kim@smuzey.ch>
2017-01-30 11:07:43 +0100

    simplify startup

--- aldryn_celery/celery.py
+++ aldryn_celery/celery.py
@@ -1,21 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
+
+from django.conf import settings
+
 import aldryn_django.startup
-import os
-import sys
 from celery import Celery
 
-# this code is very similar to aldryn_django.startup
 
-# add the current directory to pythonpath. So the project files can be read.
-BASE_DIR = os.getcwd()
-sys.path.insert(0, BASE_DIR)
-if not os.path.exists(os.path.join(BASE_DIR, 'manage.py')):
-    raise RuntimeError('make sure you are in the same directory as manage.py')
-aldryn_django.startup._setup(BASE_DIR)
-
-
-from django.conf import settings  # noqa
+aldryn_django.startup._setup(settings.BASE_DIR)
 
 app = Celery('aldryn_celery')
 

c85a04676fb5734d2a9acff3f1468a6776f64460
Kim Thoenen <kim@smuzey.ch>
2017-02-01 14:07:59 +0100

    3.1.25.3

--- aldryn_celery/__init__.py
+++ aldryn_celery/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 
-__version__ = '3.1.25.2'
+__version__ = '3.1.25.3'
 
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.

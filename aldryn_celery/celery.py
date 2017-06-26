2d62c27d82f1345bef31fd69a47e72348141e7f5
Paulo <commonzenpython@gmail.com>
2017-06-26 07:35:48 -0400

    Revert "Fixed #6 -- Setup the django registry on load (#10)"
    This reverts commit 7b0b40257bc5ecfe221060e3f96a53141e727e42.

--- aldryn_celery/celery.py
+++ aldryn_celery/celery.py
@@ -1,22 +1,17 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 
-import os
-import sys
 import aldryn_django.startup
 
 from celery import Celery
 
-from django.conf import settings
 
-# Adds the current directory to python path.
-# This is required for django to find the settings module
-sys.path.insert(0, os.getcwd())
+# `path` is not used anymore but still required
+aldryn_django.startup._setup(path=None)
 
-# Sets DJANGO_SETTINGS_MODULE environment variable
-# Calls django.setup() to setup app registry
-aldryn_django.startup.setup(path=None)
+from django.conf import settings  # noqa
 
 app = Celery('aldryn_celery')
-app.config_from_object(settings)
+
+app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
